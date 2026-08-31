1d70fc48706ef182be6e6f605bc3797704a91a35
olaoladunni6-cyber <olaoladunni6@gmail.com>
2026-08-31 16:16:54 +0300

    Hotel alert service worker

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-self.addEventListener("fetch", () => {});
+self.addEventListener("install",function(){self.skipWaiting();});
+self.addEventListener("activate",function(e){e.waitUntil(self.clients.claim());});
+self.addEventListener("notificationclick",function(e){
+  e.notification.close();
+  e.waitUntil(clients.matchAll({type:"window",includeUncontrolled:true}).then(function(list){
+    if(list&&list[0])return list[0].focus();
+    return clients.openWindow("./u.html");
+  }));
+});
