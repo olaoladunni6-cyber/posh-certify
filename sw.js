b9bb8c4a65cdfcf412e83618a18244e05e4e7623
olaoladunni6-cyber <olaoladunni6@gmail.com>
2026-09-01 14:31:12 +0300

    Service worker opens phone.html and handles push

--- sw.js
+++ sw.js
@@ -4,6 +4,13 @@ self.addEventListener("notificationclick",function(e){
   e.notification.close();
   e.waitUntil(clients.matchAll({type:"window",includeUncontrolled:true}).then(function(list){
     if(list&&list[0])return list[0].focus();
-    return clients.openWindow("./u.html");
+    return clients.openWindow("./phone.html");
   }));
+});
+self.addEventListener("push",function(e){
+  var data={title:"Posh Manager",body:"New hotel update"};
+  try{if(e.data)data=Object.assign(data,e.data.json());}catch(err){
+    try{data.body=e.data.text();}catch(e2){}
+  }
+  e.waitUntil(self.registration.showNotification(data.title,{body:data.body,tag:data.tag||"posh-push",renotify:true}));
 });
